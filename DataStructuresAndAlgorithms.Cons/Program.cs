ce86e98b280805fda610bc14e04307176028857d
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-07 09:40:20 +0200

    Add project files.

---
+++ DataStructuresAndAlgorithms.Cons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithms.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
