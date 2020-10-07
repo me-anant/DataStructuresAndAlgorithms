e39575a9c369c82d2feea9a4e810b31884341c8c
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-07 11:08:41 +0200

    Add basic custom stack

--- DataStructures.Lib/MyStack.cs
+++ DataStructures.Lib/MyStack.cs
@@ -1,8 +1,57 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace DataStructures.Lib
 {
-    public class MyStack
+    public class MyStack : IEnumerable
     {
+        private static object[] array;
+        public int Count { get; set; } = 0;
+
+        public MyStack()
+        {
+            array = new object[Count];
+        }
+
+        public MyStack(int capacity)
+        {
+            Count = capacity;
+            array = new object[capacity];
+        }
+
+        public virtual void Push(object obj)
+        {
+            Count++;
+            object[] oldArray = new object[Count];
+            Array.Copy(array, 0, oldArray, 0, Count - 1);
+
+            object[] newArray = new object[Count];
+            array = newArray;
+
+            array[0] = obj;
+            Array.Copy(oldArray, 0, array, 1, Count - 1);
+        }
+
+        public virtual object Pop()
+        {
+            if (Count == 0) throw new InvalidOperationException("My stack or rather your stack is empty mate.");
+            Count--;
+            object[] newArray = new object[Count];
+            var obj = array[0];
+            Array.Copy(array, 1, newArray, 0, Count);
+            array = newArray;
+            return obj;
+        }
+
+        public virtual object Peek()
+        {
+            return array[0];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return array.AsEnumerable().GetEnumerator();
+        }
     }
 }
