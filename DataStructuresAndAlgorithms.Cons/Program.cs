dea644d3c16888c38566a286fd7774feb51679bb
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-08 15:36:50 +0200

    Update Program.cs

--- DataStructuresAndAlgorithms.Cons/Program.cs
+++ DataStructuresAndAlgorithms.Cons/Program.cs
@@ -1,15 +1,28 @@
-﻿using DataStructures.Lib.Stacks;
+﻿using DataStructures.Lib.Arrays;
+using DataStructures.Lib.Stacks;
 using DataStructuresAndAlgorithms.Api.Services;
+using System;
 
 namespace DataStructuresAndAlgorithms.Cons
 {
     class Program
     {
+        private static StackService _stackService;
+        private static ArrayService _arrayService;
+
         static void Main(string[] args)
         {
-            // pass your stack class as an argument in the below method
-            // and run the DataStructuresAndAlgorithms.Cons project to test.
-            // StackService.CompareStackResult(new MyStack());
+            _stackService = new StackService();
+            _arrayService = new ArrayService();
+
+            // Pass your stack class as an argument in the below method.
+            // Run the DataStructuresAndAlgorithms.Cons project to test.
+            //_stackService.CompareStackResult(new MyStack());
+
+            // Pass your dynamic array class as an argument in the below method.
+            // For testing purposes use int for your array type arguments.
+            // Run the DataStructuresAndAlgorithms.Cons project to test.
+            //_arrayService.CompareArrayResult(new MyDynamicArray<int>());
         }
     }
 }
