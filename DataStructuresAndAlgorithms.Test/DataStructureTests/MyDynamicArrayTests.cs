f49b67ea4e5b455e5de2df0e22bce29b8c36b426
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-10 17:13:41 +0200

    Rename Test classes

---
+++ DataStructuresAndAlgorithms.Test/DataStructureTests/MyDynamicArrayTests.cs
@@ -0,0 +1,36 @@
+﻿using DataStructures.Lib.Arrays;
+using DataStructuresAndAlgorithms.Api.Services;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DataStructuresAndAlgorithms.Test.DataStructureTests
+{
+    public class MyDynamicArrayTests
+    {
+        private readonly ArrayService _arrayService;
+        public MyDynamicArrayTests()
+        {
+            _arrayService = new ArrayService();
+        }
+
+        [Fact]
+        public void MyDynamicArray_Equals_OriginalArray()
+        {
+            List<int> originalArray = _arrayService.CreateArray(new List<int>()) as List<int>;
+            MyDynamicArray<int> myDynamicArray = _arrayService.CreateArray(new MyDynamicArray<int>()) as MyDynamicArray<int>;
+
+            Assert.Equal(myDynamicArray, originalArray);
+        }
+
+        [Fact]
+        public void MyDynamicArray_NotEquals_OriginalArray()
+        {
+            List<int> originalArray = _arrayService.CreateArray(new List<int>()) as List<int>;
+            MyDynamicArray<int> myDynamicArray = _arrayService.CreateArray(new MyDynamicArray<int>()) as MyDynamicArray<int>;
+
+            myDynamicArray.Clear();
+
+            Assert.NotEqual(myDynamicArray, originalArray);
+        }
+    }
+}
