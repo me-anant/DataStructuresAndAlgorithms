1a6004cae5fca8a9a64bc872566b4a3536cc4595
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-10 17:16:12 +0200

    Update Program.cs

--- DataStructuresAndAlgorithms.Cons/Program.cs
+++ DataStructuresAndAlgorithms.Cons/Program.cs
@@ -1,28 +1,10 @@
-﻿using DataStructures.Lib.Arrays;
-using DataStructures.Lib.Classes;
-using DataStructures.Lib.LinkedLists;
-using DataStructures.Lib.Stacks;
-using DataStructuresAndAlgorithms.Api.Services;
-using System;
-using System.Collections.Generic;
-
-namespace DataStructuresAndAlgorithms.Cons
+﻿namespace DataStructuresAndAlgorithms.Cons
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LinkedList<int> vs = new LinkedList<int>();
-            vs.AddLast(5);
-            vs.AddLast(5);
-            vs.AddLast(5);
-            vs.AddLast(10);
-            vs.Remove(5);
 
-            foreach (var item in vs)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
