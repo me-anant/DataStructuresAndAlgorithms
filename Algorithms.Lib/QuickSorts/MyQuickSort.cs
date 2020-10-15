191cba344bec3f156908f842f90289f34a36c655
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-15 18:27:48 +0200

    Update MyQuickSort.cs

--- Algorithms.Lib/QuickSorts/MyQuickSort.cs
+++ Algorithms.Lib/QuickSorts/MyQuickSort.cs
@@ -4,86 +4,59 @@ namespace Algorithms.Lib.QuickSorts
 {
     public static class MyQuickSort
     {
-        private static int _counter = 0;
-
-        public static int[] QuickSort(this int[] array)
+        public static T[] QuickSort<T>(this T[] array)
         {
             ProcessQuickSort(array, 0, array.Length);
 
-            _counter = 0;
             return array;
         }
 
-        private static void ProcessQuickSort(int[] array, int bottom, int top)
+        private static void ProcessQuickSort<T>(T[] array, int bottom, int top)
         {
             if (bottom < top)
             {
-                dynamic partitionIndex = Partition(array, bottom, top);
+                int partitionIndex = Partition(array, bottom, top);
 
-                ProcessQuickSort(array, partitionIndex.Bottom, partitionIndex.Top);
-                ProcessQuickSort(array, partitionIndex.Bottom + 1, partitionIndex.Top);
+                ProcessQuickSort(array, bottom, partitionIndex - 1);
+                ProcessQuickSort(array, partitionIndex, top);
             }
-
-            return;
         }
 
-        private static object Partition(int[] array, int bottom, int top)
+        private static int Partition<T>(T[] array, int bottom, int top)
         {
-            if(_counter >= array.Length)
-            {
-                return new
-                {
-                    Bottom = 0,
-                    Top = 0
-                };
-            }
-
-            int[] arr = new int[top - bottom];
+            T[] arr = new T[top - bottom];
             Array.Copy(array, bottom, arr, 0, top - bottom);
 
-            int midPoint = (int)Math.Ceiling((float)arr.Length / 2);
+            // het heeft met je pivot index te maken...
+            int pivotIndex = (int)Math.Ceiling((float)(arr.Length / 2));
+            T pivot = arr[pivotIndex];
 
-            if (midPoint <= 1)
+            int partitionIndex = bottom;
+
+            for (int i = bottom; i < top; i++)
             {
-                return new
-                {
-                    Bottom = top,
-                    Top = array.Length
-                };
-            }
+                var current = array[i] as IComparable<T>;
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] == arr[midPoint]) continue;
-
-                if (arr[i] > arr[midPoint] && midPoint > i)
+                if (current.CompareTo(pivot) < 0)
                 {
-                    var bigger = arr[i];
-                    arr[i] = arr[midPoint];
-                    arr[midPoint] = bigger;
-                    midPoint = i;
-                }
-
-                if (arr[i] < arr[midPoint] && midPoint < i)
-                {
-                    var smaller = arr[i];
-                    var midPointVal = arr[midPoint];
-                    arr[midPoint] = smaller;
-                    var next = arr[midPoint + 1];
-                    arr[midPoint + 1] = midPointVal;
-                    if (i != midPoint + 1) arr[i] = next;
-                    midPoint++;
+                    array[i] = array[partitionIndex];
+                    array[partitionIndex] = (T)current;
+                    if ((bottom + pivotIndex).Equals(partitionIndex))
+                    {
+                        pivotIndex = i;
+                    }
+                    partitionIndex++;
                 }
             }
 
-            Array.Copy(arr, 0, array, bottom, top - bottom);
-            _counter++;
-
-            return new
+            T oldBig = array[partitionIndex];
+            if (!pivot.Equals(oldBig))
             {
-                Bottom = bottom,
-                Top = midPoint + 1
-            };
+                array[partitionIndex] = pivot;
+                array[bottom + pivotIndex] = oldBig;
+            }
+
+            return partitionIndex + 1;
         }
     }
 }
