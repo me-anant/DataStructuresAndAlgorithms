0335f0d10635d07f22a1754a84d2f31024cbc89a
Jens Deweirdt <jensdeweirdt@outlook.be>
2020-10-17 15:03:53 +0200

    Add Binary Search and Tests

---
+++ Algorithms.Lib/Searching/BinarySearches/MyBinarySearch.cs
@@ -0,0 +1,47 @@
+﻿using Algorithms.Lib.Sorting.MergeSorts;
+using System;
+
+namespace Algorithms.Lib.Searching.BinarySearches
+{
+    public static class MyBinarySearch
+    {
+        public static T BinarySearch<T>(this T[] array, T searchKey)
+        {
+            if (typeof(T).GetProperties().Length > 0)
+                throw new ArgumentException("This method can only be called on arrays of predefined data types.", nameof(array));
+
+            array.MergeSort();
+            return Find(array, searchKey);
+        }
+
+        private static T Find<T>(T[] array, T searchKey)
+        {
+            if (array.Length > 0)
+            {
+                int center = array.Length / 2;
+                IComparable<T> centerValue = array[center] as IComparable<T>;
+
+                if (centerValue.CompareTo(searchKey) == 0)
+                {
+                    return (T)centerValue;
+                }
+
+                if (centerValue.CompareTo(searchKey) < 0)
+                {
+                    T[] right = new T[array.Length - center - 1];
+                    Array.Copy(array, center + 1, right, 0, array.Length - center - 1);
+                    return Find(right, searchKey);
+                }
+
+                if (centerValue.CompareTo(searchKey) > 0)
+                {
+                    T[] left = new T[center];
+                    Array.Copy(array, 0, left, 0, center);
+                    return Find(left, searchKey);
+                }
+            }
+
+            return default;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms.Test/AlgorithmTests/Searching/BinarySearches/MyBinarySearchTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Lib.Searching.BinarySearches;
+using System;
+using Xunit;
+
+namespace DataStructuresAndAlgorithms.Test.AlgorithmTests.Searching.BinarySearches
+{
+    public class MyBinarySearchTests
+    {
+        [Fact]
+        public void MyBinarySearch_Equals_One()
+        {
+            int[] arr = { 350, 78, 5, 4, 80, 65, 17, 1, 9, 53 };
+
+            int key = arr.BinarySearch(1);
+
+            Assert.Equal(1, key);
+        }
+
+        [Fact]
+        public void MyBinarySearch_Returns_0()
+        {
+            int[] arr = { 350, 78, 5, 4, 80, 65, 17, 1, 9, 53 };
+
+            int key = arr.BinarySearch(3);
+
+            Assert.Equal(0, key);
+        }
+    }
+}
